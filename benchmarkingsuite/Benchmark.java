a0881c07f8634f4d82025523d4debb17f5622b18
ritickgoenka <rgoenka@ec.iitr.ac.in>
2020-06-11 20:27:54 +0530

    changed queryparser

--- benchmarkingsuite/Benchmark.java
+++ benchmarkingsuite/Benchmark.java
@@ -2,6 +2,8 @@ package benchmarkingsuite;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -15,7 +17,12 @@ import java.nio.charset.StandardCharsets;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.queryparser.simple.SimpleQueryParser;
+import org.apache.lucene.queryparser.simple.SimpleQueryParser;
+
 import org.apache.lucene.document.Document;
+
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.flexible.standard.*;
 
 
 
@@ -26,10 +33,11 @@ import org.apache.lucene.monitor.*;
 public class Benchmark{
   public Monitor monitor;
   public Analyzer analyzer;
-  public SimpleQueryParser queryparser;
+  public QueryParser queryparser;
   public long doc_count = 0;
   public long match_count = 0;
   public long search_time = 0;
+  public HashMap<String,Integer>query_match_cnt = new HashMap<String,Integer>();
 
 
   // create a new Benchmark object with StandardAnalyzer
@@ -41,11 +49,17 @@ public class Benchmark{
   public Benchmark( Analyzer analyzer) throws IOException{
     this.analyzer = analyzer;
     this.monitor = new Monitor(this.analyzer);
-    this.queryparser = new SimpleQueryParser(this.analyzer,"text");
+    this.queryparser = new QueryParser("",this.analyzer);
+
   }
 
   // create a new Report of the matches done till now
   public Report getReport()throws IOException{
+    for (String name: query_match_cnt.keySet()){
+            String key = name.toString();
+            String value = query_match_cnt.get(name).toString();
+            System.out.println(key + " " + value);
+    }
     return new Report(doc_count,monitor.getQueryCount(),match_count ,search_time);
   }
 
@@ -58,7 +72,14 @@ public class Benchmark{
         String queryString;
         while ((queryId = br.readLine()) != null) {
             queryString = br.readLine();
+            // Query query = simplequeryparser.parse(queryString);
+            // queryString = query.toString();
+            // query = queryparser.parse(queryString);
+            // StandardQueryParser parser = new StandardQueryParser(analyzer);
             Query query = queryparser.parse(queryString);
+
+            query_match_cnt.put(queryId,0);
+            System.out.println(query);
             queries.add(new MonitorQuery(queryId, query));
         }
     }
@@ -79,6 +100,7 @@ public class Benchmark{
     for(int i=0; i<contents.length; i++) {
        dp.addJsonDocument(message_path+"/"+contents[i],doc);
        if((i+1)%batch_size == 0||i == contents.length-1){
+         System.out.println(i);
          Document docs[] = new Document[doc.size()];
          docs = doc.toArray(docs);
          doc.clear();
@@ -89,10 +111,20 @@ public class Benchmark{
 
   // match all the docments present in docs array
   public void execute(Document[] docs) throws IOException{
+
     MultiMatchingQueries<QueryMatch> matches = monitor.match(docs,QueryMatch.SIMPLE_MATCHER);
     doc_count += matches.getBatchSize();
     for(int i=0;i<matches.getBatchSize();i++){
       match_count+=matches.getMatchCount(i);
+      // if(matches.getMatchCount(i)!=551)
+      // {
+      //   System.out.println("yayayay");
+      //   System.out.println(matches.getMatchCount(i));
+      // }
+      Collection<QueryMatch> col=matches.getMatches(i);
+      for (QueryMatch docMatches : col) {
+        query_match_cnt.put(docMatches.getQueryId(),query_match_cnt.get(docMatches.getQueryId())+1);
+      }
     }
     search_time += matches.getSearchTime();
   }
